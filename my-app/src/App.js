b7233be59d1ff7190aaa44a3073474e333ff5af4
Ama Namin <markernest322@gmail.com>
2025-05-16 00:57:20 +0800

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./Components/SignUp";
+import Reserved from "./Components/Reserved";
+import LogIn from "./Components/LogIn";
+import Home from "./Components/Home";
+import ContactUs from "./Components/ContactUs";
+import About from "./Components/About";
+import Search from "./Components/SearchBar";
+import Navbar from "./Components/Navigation";
+import MovieDetail from "./Components/MovieDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Navbar />
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/reserved" element={<Reserved />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+ {/* new */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
